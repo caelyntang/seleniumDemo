9ae442d3e53b86df836434cbafb370d2ee5551dd
South <caelyntang@163.com>
2018-04-01 15:23:36 +0800

    this demo is dedicated for demonstrating,private only!

---
+++ src/main/java/maven_testng0401/maven_testng0401/App.java
@@ -0,0 +1,13 @@
+package maven_testng0401.maven_testng0401;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/maven_testng0401/maven_testng0401/AppTest.java
@@ -0,0 +1,10 @@
+package maven_testng0401.maven_testng0401;
+import org.testng.annotations.Test;;
+
+
+public class AppTest {
+	@Test
+	public void Test() {
+		System.out.println("============************>>>>>>TEST");
+	}
+}
