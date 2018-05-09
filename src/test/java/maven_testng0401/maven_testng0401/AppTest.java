3f71e7eaeb8329a0b695e7fd3ccaf1a2e70ab5ee
South <caelyntang@163.com>
2018-05-09 15:40:11 +0800

    test050915:40

--- src/test/java/maven_testng0401/maven_testng0401/AppTest.java
+++ src/test/java/maven_testng0401/maven_testng0401/AppTest.java
@@ -1,10 +1,25 @@
 package maven_testng0401.maven_testng0401;
-import org.testng.annotations.Test;;
+import org.testng.annotations.Test;
+import org.apache.commons.codec.digest.DigestUtils; 
 
 
 public class AppTest {
 	@Test
 	public void Test() {
-		System.out.println("============************>>>>>>TEST");
+		
+		
+		String str = "000000";  ///
+		  
+		String sign = DigestUtils.md5Hex(str);  
+		
+		System.out.println("============************>>>>>>TEST----"+sign);
+		String sign_ = sign.toLowerCase(); 
+		System.out.println("============************>>>>>>TEST----"+sign_);
+		
+		String sign_str =  DigestUtils.md5Hex(sign_); 
+		System.out.println("============************>>>>>>TEST----"+sign_str);
+		
+		
+		//System.out.println("============************>>>>>>TEST");
 	}
 }
